8d7ad46d2b6546fb27a05f44644b40555755abcb
debiancc <debiancc@users.noreply.github.com>
2023-09-09 22:06:03 +0800

    Add centerid

--- src/center.rs
+++ src/center.rs
@@ -1,9 +1,27 @@
 use geojson::{GeoJson, PointType};
 use crate::bbox::bbox;
+use crate::coord_each::coord_each;
 
 pub fn center(geojson: &GeoJson) -> PointType {
     let ext = bbox(geojson, false);
     let x = (ext[0] + ext[2]) / 2.;
     let y = (ext[1] + ext[3]) / 2.;
     vec![x, y]
+}
+
+pub fn centerid(geojson: &GeoJson) -> PointType {
+    let mut x = 0_f64;
+    let mut y = 0_f64;
+    let mut len = 0_f64;
+
+    coord_each(geojson, |coord, _, _, _, _| {
+        x += coord[0];
+        y += coord[1];
+        len += 1.;
+        true
+    }, false);
+
+    let x = x / len;
+    let y = y / len;
+    return vec![x, y]
 }
